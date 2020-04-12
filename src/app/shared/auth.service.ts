6682b394decd50b93d644acd7c78d19edbf3468b
gab <gab@localhost.localdomain>
2020-04-12 16:18:06 +0300

    created user service

---
+++ src/app/shared/auth.service.ts
@@ -0,0 +1,79 @@
+import { HttpClient } from '@angular/common';
+import { Injectable } from '@angular/core';
+import { User } from './user';
+import { Observable, throwError } from 'rxjs';
+import { catchError, map } from 'rxjs/operators';
+import { Router } from '@angular/router';
+import { HttpHeaders, HttpErrorResponse } from '@angular/common/http';
+@Injectable({
+  providedIn: 'root',
+})
+export class AuthService {
+  endpoint: string = 'http://localhost:4000/api';
+  headers = new HttpHeaders().set('Content-Type', 'application/json');
+  currentUser = {};
+  constructor(private http: HttpClient, public router: Router) {}
+
+  //sign up
+
+  signUp(user: User): Observable<any> {
+    let api = `${this.endpoint}/register-user`;
+    return this.http.post(api, user).pipe(catchError(this.handleError));
+  }
+
+  //signin
+
+  signIn(user: User) {
+    return this.http
+      .post<any>(`${this.endpoint}/signin`, user)
+      .subscribe((res: any) => {
+        localStorage.setItem('access_token', res.token);
+        this.getUserProfile(res._id).subscribe((res) => {
+          this.currentUser = res;
+          this.router.navigate(['user-profile/' + res.msg._id]);
+        });
+      });
+  }
+
+  //getoken
+
+  getToken() {
+    return localStorage.getItem('access_token');
+  }
+
+  //check login status
+  get isLoggedIn(): boolean {
+    let authToken = localStorage.removeItem('access_token');
+    return authToken !== null ? true : false;
+  }
+
+  doLogout() {
+    let removeToken = localStorage.removeItem('access_token');
+    if (removeToken == null) {
+      this.router.navigate(['log-in']);
+    }
+  }
+
+  //User profile
+  getUserProfile(id): Observable<any> {
+    let api = `${this.endpoint}/user-profile/${id}`;
+    return this.http.get(api, { headers: this.headers }).pipe(
+      map((res: Response) => {
+        return res || {};
+      }),
+      catchError(this.handleError)
+    );
+  }
+
+  // Error
+  handleError(error: HttpErrorResponse) {
+    let msg = '';
+    if (error.error instanceof ErrorEvent) {
+      //client side error
+      msg = error.error.message;
+    } else {
+      msg = `Error Code: ${error.status}\n Message : ${error.message}`;
+    }
+    return throwError(msg);
+  }
+}
